73816a10d6beeae388e6c69d9cef617f765e6714
ducruyy <ducruyy@127.0.0.1>
2016-01-05 09:45:08 +0100

    modif du code etape 2

--- elf_linker-1.0/etape2/test.c
+++ elf_linker-1.0/etape2/test.c
@@ -11,13 +11,19 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include"read_header.h"
+#include <sys/stat.h>
 
-/*
-*
-*/
+off_t fsize(const char *filename) {
+    struct stat st; 
+
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+
+    return -1; 
+}
 int main(int argc,char** argv){
     char *f=argv[1];
-    size_t s=argv[2];
+    size_t s=fsize(f);
     read_header(f,s);
     return(EXIT_SUCCESS);
 }
