40be2e8c0787c4995e70ee871e393b7e52eaae85
ducruyy <ducruyy@127.0.0.1>
2016-01-05 08:57:37 +0100

    ajout du code etape 2

---
+++ elf_linker-1.0/etape2/newmain.c
@@ -0,0 +1,18 @@
+/*
+*File:newmain.c
+*Author:ducruyy
+*
+*Createdon5janvier2016,08:25
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<elf.h>
+
+/*
+*
+*/
+intmain(int argc,char** argv){
+    
+    return(EXIT_SUCCESS);
+}
