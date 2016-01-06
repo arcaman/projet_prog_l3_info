992d1da55f43b90b4aaa1e78614f9b4cfe096453
ducruyy <ducruyy@127.0.0.1>
2016-01-06 09:00:53 +0100

    fin du code etape 2

--- elf_linker-1.0/sources_perso/etape2/read_header.c
+++ elf_linker-1.0/sources_perso/etape2/read_header.c
@@ -2,12 +2,12 @@
 #include "../en_tete_elf.c"
 #define MODE_BIG_ENDIAN 2
 
-Elf32_Shdr createObjectSectionheader(char* nameFile, int index){
+Elf32_Shdr createObjectSectionheader(char* nameFile, int index) {
     Elf32_Shdr shdr;
     FILE* fichierAnalyse = fopen(nameFile, "r");
     Elf32_Ehdr elfHdr = createObjectEnteteELF(nameFile);
-    fseek(fichierAnalyse, elfHdr.e_shoff + index * sizeof(Elf32_Shdr), SEEK_SET);
-    
+    fseek(fichierAnalyse, elfHdr.e_shoff + index * sizeof (Elf32_Shdr), SEEK_SET);
+
     fread(&shdr.sh_name, sizeof (Elf32_Word), 1, fichierAnalyse);
     fread(&shdr.sh_type, sizeof (Elf32_Word), 1, fichierAnalyse);
     fread(&shdr.sh_flags, sizeof (Elf32_Word), 1, fichierAnalyse);
@@ -18,7 +18,7 @@ Elf32_Shdr createObjectSectionheader(char* nameFile, int index){
     fread(&shdr.sh_info, sizeof (Elf32_Word), 1, fichierAnalyse);
     fread(&shdr.sh_addralign, sizeof (Elf32_Word), 1, fichierAnalyse);
     fread(&shdr.sh_entsize, sizeof (Elf32_Word), 1, fichierAnalyse);
-    
+
     if (elfHdr.e_ident[5] == MODE_BIG_ENDIAN) { // 5 correspondant à l'octet étant le big ou little
         shdr.sh_name = __bswap_32(shdr.sh_name);
         shdr.sh_type = __bswap_32(shdr.sh_type);
@@ -31,50 +31,51 @@ Elf32_Shdr createObjectSectionheader(char* nameFile, int index){
         shdr.sh_addralign = __bswap_32(shdr.sh_addralign);
         shdr.sh_entsize = __bswap_32(shdr.sh_entsize);
     }
-    
+
     fclose(fichierAnalyse);
     return shdr;
 }
 
-void read_section_header(char * filename, size_t size){
+void read_section_header(char * filename, size_t size) {
     Elf32_Ehdr elfHdr;
     Elf32_Shdr sectHdr;
     Elf32_Shdr strTab;
     uint32_t idx;
     // read ELF header, first thing in the file
     elfHdr = createObjectEnteteELF(filename);
-    printf("nb sections : %i\n",elfHdr.e_shnum); 
+    printf("nb sections : %i\n", elfHdr.e_shnum);
     strTab = createObjectSectionheader(filename, elfHdr.e_shstrndx);
     FILE* fichier = fopen(filename, "r");
-    fseek(fichier,strTab.sh_offset, SEEK_SET);
+    fseek(fichier, strTab.sh_offset, SEEK_SET);
     char * str = malloc(strTab.sh_size);
-    for(idx=0;idx<strTab.sh_size;idx++){
-        str[idx]=fgetc(fichier);
+    for (idx = 0; idx < strTab.sh_size; idx++) {
+        str[idx] = fgetc(fichier);
     }
-    
+
     // read all section headers
-    for (idx = 0; idx < elfHdr.e_shnum; idx++){
-        
+
+    for (idx = 0; idx < elfHdr.e_shnum; idx++) {
+
         sectHdr = createObjectSectionheader(filename, idx);
-        printf("SECTION numero %i : \n",idx);
+        printf("SECTION numero %i : \n", idx);
         printf("name : ");
-        int i=sectHdr.sh_name;
-        while(str[i]!='\0'){
-            printf("%c",str[i]);
+        int i = sectHdr.sh_name;
+        while (str[i] != '\0') {
+            printf("%c", str[i]);
             i++;
         }
         printf("\n");
-        printf("type : %u\n",sectHdr.sh_type); //a remplacer par leur équivalent
-        printf("size : %u offset : %u\n",sectHdr.sh_size,sectHdr.sh_offset);
-        printf("address : %u\n",sectHdr.sh_addr);
-        printf("flags : %x\n",sectHdr.sh_flags);
-        if(sectHdr.sh_entsize!=0){
-            printf("taille des entrees prefixee a %u bits \n",sectHdr.sh_entsize);
+        printf("type : %u\n", sectHdr.sh_type); //a remplacer par leur équivalent
+        printf("size : %u offset : %u\n", sectHdr.sh_size, sectHdr.sh_offset);
+        printf("address : %u\n", sectHdr.sh_addr);
+        printf("flags : %x\n", sectHdr.sh_flags);
+        if (sectHdr.sh_entsize != 0) {
+            printf("taille des entrees prefixee a %u bits \n", sectHdr.sh_entsize);
         }
-        
-        
+
+
         printf("\n");
-        
+
     }
     fclose(fichier);
 }
