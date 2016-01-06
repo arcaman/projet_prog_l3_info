6e9b0d6f4fac595265605450e0874d3c2b5a1509
rocheroh <rocheroh@127.0.0.1>
2016-01-06 13:57:39 +0100

    début étape 4

---
+++ elf_linker-1.0/sources_perso/etape4/test.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   test.c
+ * Author: rocheroh
+ *
+ * Created on 6 janvier 2016, 13:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
